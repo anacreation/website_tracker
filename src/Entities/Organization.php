f96f36d6294d1d345729c8294b744bd9e815b455
Xavier Au <xavier.au@anacreation.com>
2020-10-19 10:06:57 +0800

    update

--- src/Entities/Organization.php
+++ src/Entities/Organization.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class Organization extends Model
 {
@@ -30,7 +31,7 @@ class Organization extends Model
 
     // region Helpers
 
-    public function inOrganization(object $entity): bool {
+    public function hasEntity(object $entity): bool {
         return $this->entities()
                     ->where('entity_type',
                             get_class($entity))
@@ -39,28 +40,77 @@ class Organization extends Model
                     ->exists();
     }
 
+    public static function isInSameOrganization(object $entity_1, object $entity_2): bool {
+        $entity1ClassName = get_class($entity_1);
+        $entity2ClassName = get_class($entity_2);
+
+        $query = DB::raw("SELECT organization_id
+FROM entity_organization
+WHERE entity_type = '{$entity1ClassName}' AND entity_id = {$entity_1->id}
+
+INTERSECT
+
+SELECT organization_id
+FROM entity_organization
+WHERE entity_type = '{$entity2ClassName}' AND entity_id = {$entity_2->id} ");
+
+        return count(DB::select($query)) > 0;
+    }
+
     public function entities(): HasMany {
         return $this->hasMany(Entity::class);
     }
 
     public static function getTree(int $organization_id = null): Collection {
-        $instance = new self;
 
-        $query = $organization_id === null ?
-            $instance->where('parent_id',
-                             null):
-            $instance->where('id',
-                             $organization_id);
-
-        return $query->with('children')
-                     ->get()
-                     ->each(fn($o) => $instance->loadChildren($o));
+        return Organization::whereIn('id',
+                                     collect(DB::select(Organization::getRecursiveSql($organization_id)))
+                                         ->pluck('id'))
+                           ->get();
     }
 
-    private function loadChildren(Organization $organization) {
-        $organization->load('children');
-        $organization->children
-            ->each(fn($o) => $this->loadChildren($o));
+    public static function getRecursiveSql(int $parent_id = null) {
+
+        if($parent_id !== null) {
+            return DB::raw("WITH RECURSIVE organization_paths AS (
+	SELECT
+		id,
+		parent_id
+	FROM organizations
+	WHERE parent_id = {$parent_id} OR id = {$parent_id}
+	UNION ALL
+	SELECT
+		o.id,
+		o.parent_id
+	FROM
+		organizations AS o
+		INNER JOIN organization_paths op ON op.id = o.parent_id
+)
+SELECT
+	id
+FROM
+	organization_paths op");
+        }
+
+        return DB::raw("WITH RECURSIVE organization_paths AS (
+	SELECT
+		id,
+		parent_id
+	FROM organizations
+	WHERE parent_id IS NULL
+	UNION ALL
+	SELECT
+		o.id,
+		o.parent_id
+	FROM
+		organizations AS o
+		INNER JOIN organization_paths op ON op.id = o.parent_id
+)
+SELECT
+	id
+FROM
+	organization_paths op");
+
     }
 
     // endregion
--- src/Traits/InOrganization.php
+++ src/Traits/InOrganization.php
@@ -19,11 +19,13 @@ namespace Anacreation\Organization\Traits;
 use Anacreation\Organization\Entities\Entity;
 use Anacreation\Organization\Entities\Organization;
 use Exception;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Support\Facades\DB;
 
 trait InOrganization
 {
-    protected function organizations(): MorphToMany {
+    public function organizations(): MorphToMany {
         return $this->morphToMany(Organization::class,
                                   'entity',
                                   'entity_organization')
@@ -34,14 +36,36 @@ trait InOrganization
     }
 
     public function isInOrganization(Organization $organization): bool {
-        return $organization->inOrganization($this);
+        return $organization->hasEntity($this);
     }
 
     public function assignToOrganization(
         Organization $organization, bool $include_sub_org = false): void {
         try {
-            $this->organizations()->attach($organization,['include_sub_org'=>$include_sub_org]);
+            $this->organizations()->attach($organization,
+                                           ['include_sub_org' => $include_sub_org]);
         } catch (Exception $e) {
         }
+    }
+
+    public function scopeInOrganization(
+        Builder $q, Organization $organization, bool $include_sub_org = false): Builder {
+
+        if($include_sub_org) {
+            return $q->whereHas('organizations',
+                function($sq) use ($organization) {
+                    return $sq->whereIn('organization_id',
+                                        collect(DB::select(Organization::getRecursiveSql($organization->id)))
+                                            ->pluck('id')->values()
+                                            ->toArray());
+                });
+        }
+
+        return $q->whereHas('organizations',
+            function($sq) use ($organization) {
+                return $sq->where('organization_id',
+                                  $organization->id);
+            });
+
     }
 }
